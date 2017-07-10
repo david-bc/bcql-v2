53761daf312d4d7b8b6e60def84db6bda4ed201a
David Esposito <david.esposito@bettercloud.com>
2017-07-10 17:09:53 -0400

    handling unknown collections
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

--- src/components/GridPage.js
+++ src/components/GridPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import { withRouter } from 'react-router-dom'
+import { withProps, branch, renderComponent, compose } from 'recompose'
 import Configurations from '../config/index'
 import Service from '../service'
 
 import { Table, TableHeader } from 'react-mdl';
 import ContentWrapper from './ContentWrapper';
 import AdvancedSearchForm from './AdvancedSearchForm'
+import SomethingWentWrong from './SomethingWentWrong'
 
-const GridPage = ({ match }) => {
+let GridPage = ({ match }) => {
   const collection = match.path.substring(1);
   const config = Configurations[collection];
   const columns = config.grid.columns;
@@ -40,6 +43,24 @@ const GridPage = ({ match }) => {
   );
 }
 
-GridPage.propTypes = {};
+GridPage = compose(
+  withProps(props => {
+    const match = props.match;
+    const collection = match.path.substring(1);
+    const config = Configurations[collection];
+    const collectionService = Service[collection];
+    let columns = null;
+    let rows = null;
+    if (!_.isNil(config) && !_.isNil(collectionService)) {
+      columns = config.grid.columns;
+      rows = Service[collection].fetchAll().data;
+    }
+    return { columns, rows };
+  }),
+  branch(
+    ({ columns }) => _.isNil(columns),
+    renderComponent(SomethingWentWrong)
+  )
+)(GridPage)
 
 export default withRouter(GridPage);
---
+++ src/components/SomethingWentWrong.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SomethingWentWrong = ({}) => {
+  return (
+    <div className="SomethingWentWrong" style={{ textAlign: 'center' }}>
+      <h1>Something went wrong :(</h1>
+    </div>
+  );
+}
+
+SomethingWentWrong.propTypes = {};
+
+export default SomethingWentWrong;
