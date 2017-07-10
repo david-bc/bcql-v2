afacebde5fbad7f375f2705e0be080cd82708c32
Michael Stone <michaelstone@bc-mstone-mbpr-61084.local>
2017-07-10 18:08:27 -0400

    ¯\_(ツ)_/¯

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 import users from './Users';
 import groups from './Groups';
+import assets from './Assets';
 import auditlogs from './Audits';
 
 export default {
   users,
   groups,
+  assets,
   auditlogs
 };
--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,10 @@
 import users from './Users';
 import groups from './Groups';
+import assets from './Assets';
 import auditlogs from './Audits';
 export default {
   users,
   groups,
+  assets,
   auditlogs
 }
-
